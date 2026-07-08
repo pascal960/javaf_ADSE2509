f0431716c2e2e00a9738b56233bea0fb75edc511
unknown <pkeya2006@gmail.com>
2026-07-09 01:43:15 +0300

    Added src hello.java example

---
+++ src/hello.java
@@ -0,0 +1,11 @@
+/** 
+ * First java program to display "Hello world!" on the console/monitor. 
+ * @author Nyanjui 
+ */ 
+public class hello { //main method begins program execution 
+    public static void main(String[] args) { 
+        System.out.println("Hello World!"); 
+        System.out.println("Hello World!"); 
+    } 
+}
+ 
